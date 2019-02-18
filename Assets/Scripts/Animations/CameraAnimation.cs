743e6ee3b71f9a08bb65a68bf06feec033834da0
Damagae <daphnegm.rose0gmail.com>
2019-02-18 14:42:49 +0100

    camera implemented

--- Assets/Scripts/Animations/CameraAnimation.cs
+++ Assets/Scripts/Animations/CameraAnimation.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class CameraAnimation : MonoBehaviour {
 
-	public Transform[] positions;
-	private Transform currentPosition;
+	private Animator animator;
 
 	// DEBUG
-	public int indexPos = 0;
+	public int indexPos = 1;
 
 	// Use this for initialization
 	void Start () {
-		currentPosition = positions[indexPos];
-		Debug.Log(currentPosition.position);
+		animator = GetComponent<Animator>();
+		animator.SetInteger("index", indexPos);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (currentPosition != positions[indexPos]) {
-			currentPosition = positions[indexPos];
-			transform.position = currentPosition.position;
-			Debug.Log(currentPosition.position);
+		if (animator.GetInteger("index") != indexPos) {
+			animator.SetInteger("index", indexPos);
 		}
 	}
 
 	public void SetCameraIndex(int index) {
-		if (index < positions.Length) {
-			indexPos = index;
-		}
+		indexPos = index;
 	}
 }

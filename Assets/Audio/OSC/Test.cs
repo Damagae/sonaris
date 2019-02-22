107fb158d9e9c042caa826f75b2c585576547e98
Damagae <daphnegm.rose0gmail.com>
2019-02-22 12:30:07 +0100

    simple osc receiving

---
+++ Assets/Audio/OSC/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityOSC;
+
+public class Test : MonoBehaviour {
+
+	private OSCReciever manager;
+	public int port = 9001;
+
+	// Use this for initialization
+	void Start () {
+		manager = new OSCReciever();
+		manager.Open(9001);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (manager.hasWaitingMessages()) {
+			var msg = manager.getNextMessage();
+			Debug.Log(msg);
+		}
+	}
+}

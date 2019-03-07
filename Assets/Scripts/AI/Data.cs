6629999359d3b9fd4fe5db3341ac93b81eb017f6
Damagae <daphnegm.rose0gmail.com>
2019-03-07 11:17:31 +0100

    Taining OK

--- Assets/Scripts/AI/Data.cs
+++ Assets/Scripts/AI/Data.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Data {
 
@@ -20,13 +21,17 @@ public class Data {
 
 	public void Add(string[] keys, double[] values)
 	{
-		AssociativeArray<double> aa = new AssociativeArray<double>();
-		for (int i = 0; i < keys.Length; ++i)
-		{
-			aa.Add(keys[i], values[i]);
+		if (keys.Length != values.Length) {
+			Debug.Log("keys length different than values length");
+		} else {
+			AssociativeArray<double> aa = new AssociativeArray<double>();
+			for (int i = 0; i < keys.Length; ++i)
+			{
+				aa.Add(keys[i], values[i]);
+			}
+			data.Add(aa);
+			length = data.Count;
 		}
-		data.Add(aa);
-		length = data.Count;
 	}
 
 	public void Add(string key, double value)
